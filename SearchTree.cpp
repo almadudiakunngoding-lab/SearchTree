c20507dcfac27ff858ac4aa6ec28798d19f6e6c7
almaudud <almadudiakunngoding@gmail.com>
2026-06-11 12:36:05 +0700

    menambahkan void insert

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -23,3 +23,39 @@ public:
 class BinaryTree
 {
 public: 
+    Node *ROOT;
+
+    BinaryTree()
+    {
+        ROOT = NULL; // Initializing ROOT to nul
+    }
+
+    // Insert a node in the binary search tree
+    void insert(string element)
+    {
+        Node *newNode = new Node(element, NULL, NULL);
+
+        newNode->info = element;
+        newNode->leftchild = NULL;
+        newNode->rightchild = NULL;
+
+        Node *parent = NULL;
+        Node *currentNode = NULL;
+
+        search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT = newNode;
+            return;
+        }
+
+        if (element < parent->info)
+        {
+            parent->leftchild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent->rightchild = newNode;
+        }
+    }
