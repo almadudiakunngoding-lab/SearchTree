7b3439a6aaccc92abf345cb583d8a4c586a797b0
almaudud <almadudiakunngoding@gmail.com>
2026-06-11 12:40:28 +0700

    menambahkan if ROOT dan if ptr

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -114,4 +114,18 @@ public:
     void postorder(Node *ptr)
     {
         // perfoms the postorder traveersal of the tree
-x
+
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
