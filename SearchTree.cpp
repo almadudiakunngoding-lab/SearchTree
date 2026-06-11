43392d5540024a08d824ba579ea5cb58fe19999e
almaudud <almadudiakunngoding@gmail.com>
2026-06-11 12:38:23 +0700

    menambahkan void preoeder

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -94,3 +94,11 @@ public:
             inorder(ptr->rightchild);
         }
     }
+
+    void preorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout <<"Tree is empty" <<  endl;
+            return;
+        }
