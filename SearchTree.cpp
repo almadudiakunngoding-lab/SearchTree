21862ac7bbefea2b90ca4193b8b09f8a0cc17454
almaudud <almadudiakunngoding@gmail.com>
2026-06-11 12:33:19 +0700

    menambahkan class ninaryTree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -14,3 +14,11 @@ public:
     // Constructor for the node class
     Node(string i, Node *l, Node *r)
     {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
+class BinaryTree
+{
